738d775f08ed8744f2e82c41638965ab12d6816b
UNF4Z3D21 <76532002+UNF4Z3D21@users.noreply.github.com>
2023-10-06 15:53:24 +1100

    Unit Testing

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        # Set up the test client
+        self.app = app.test_client()
+        self.app.testing = True 
+
+    def test_hello_world(self):
+        # Send GET request to the app and get the response
+        response = self.app.get('/')
+        
+        # Assert the status code and the response data
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), 'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
